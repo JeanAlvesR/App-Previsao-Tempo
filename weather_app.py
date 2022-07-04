18f4032eee8cbca3c017c43e7db644d62167d405
Jean Alves Rocha <96813984+JeanAlvesR@users.noreply.github.com>
2022-07-04 09:43:40 -0300

    Convertendo str da requisicao para dict usando Json Loads. Utilizando longitude e latitude via api.

--- weather_app.py
+++ weather_app.py
@@ -1,8 +1,14 @@
 import requests
-
+import json
+import pprint as pt
 r = requests.get('http://www.geoplugin.net/json.gp')
 
 if(r.status_code!=200):
     print('Não foi possível obter a localização.')
 else:
-    print(r.text)
+    #json.loads converte o r.text, que é uma str, para um dicionário: chave-valor.
+    localizacao = json.loads(r.text)
+    #print(pt.pprint(localizacao))
+    latitude = localizacao['geoplugin_latitude'] 
+    longitude = localizacao['geoplugin_longitude']
+    print('Latitude:',latitude,'Logitude:',longitude) 
