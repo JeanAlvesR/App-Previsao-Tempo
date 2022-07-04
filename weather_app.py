d10f431b0d81cf9ca118a3b86bdfe2f6246982e6
Jean Alves Rocha <96813984+JeanAlvesR@users.noreply.github.com>
2022-07-04 12:13:06 -0300

    Código refatorado

--- weather_app.py
+++ weather_app.py
@@ -4,47 +4,68 @@ import pprint as pt
 
 accuweatherAPIKey = 'm2zswr8SjO4dAgWSXBNKjPcVo4S4DwHN'
 
-r = requests.get('http://www.geoplugin.net/json.gp')
 
-if(r.status_code!=200):
-    print('Não foi possível obter a localização.')
-    exit()
-else:
-    ##json.loads converte o r.text, que é uma str, para um dicionário: chave-valor.
-    localizacao = json.loads(r.text)
-    ##print(pt.pprint(localizacao))
-    latitude = localizacao['geoplugin_latitude'] 
-    longitude = localizacao['geoplugin_longitude']
+def pegarCoordenadas():
 
-#Utilizando API para descobrir o código referente a latitude e longitude.
-localizationAPI = 'http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey='+accuweatherAPIKey+'&q='+latitude+'%2C%20'+longitude+'&language=pt-br'
+    r = requests.get('http://www.geoplugin.net/json.gp')
 
-rGeoPosition = requests.get(localizationAPI)
+    if(r.status_code!=200):
+        print('Não foi possível obter a localização.')
+        exit()
+    else:
+        ##json.loads converte o r.text, que é uma str, para um dicionário: chave-valor.
+        localizacao = json.loads(r.text)
+        ##print(pt.pprint(localizacao))
+        coordenadas = {}
+        coordenadas['latitude'] = localizacao['geoplugin_latitude'] 
+        coordenadas['longitude']= localizacao['geoplugin_longitude']
 
-if(rGeoPosition.status_code!=200):
-    print(rGeoPosition.status_code)
-    print('Não foi possível obter a localização')
-else:
-    #só peguei o código e a cidade, estado e país
-    r2 = json.loads(rGeoPosition.text)
-    
-    nomeLocal = r2['LocalizedName'] + ', ' + r2['AdministrativeArea']['LocalizedName'] + '. ' + r2['Country']['LocalizedName'] +'.'
+        return coordenadas
+        
+def pegarCodigo(latitude, longitude):
+    #Utilizando API para descobrir o código referente a latitude e longitude.
+    localizationAPI = 'http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey='+accuweatherAPIKey+'&q='+latitude+'%2C%20'+longitude+'&language=pt-br'
 
-    codGeoPositon = r2['Key']
-    print('Local:',nomeLocal)
+    rGeoPosition = requests.get(localizationAPI)
+
+    if(rGeoPosition.status_code!=200):
+        print(rGeoPosition.status_code)
+        print('Não foi possível obter a localização')
+    else:
+        #só peguei o código e a cidade, estado e país
+        locationResponse = json.loads(rGeoPosition.text)
+        
+        nomeLocal = locationResponse['LocalizedName'] + ', ' + locationResponse['AdministrativeArea']['LocalizedName'] + '. ' + locationResponse['Country']['LocalizedName'] +'.'
+
+        codGeoPosition = locationResponse['Key']
+        infoLocal = {'nomeLocal': nomeLocal, 'codGeoPosition':codGeoPosition}
+        return infoLocal
 
 
+def pegarTempoAgora(codGeoPosition, nomeLocal):
+
+    geoConditionURL = 'http://dataservice.accuweather.com/currentconditions/v1/'+codGeoPosition+'?apikey='+accuweatherAPIKey+'&language=pt-br'
 
 
-geoConditionURL = 'http://dataservice.accuweather.com/currentconditions/v1/'+codGeoPositon+'?apikey='+accuweatherAPIKey+'&language=pt-br'
+    rTempLocation  = requests.get(geoConditionURL)
 
+    if rTempLocation.status_code !=200:
+        print('Não foi possível conectar ao serviço de temperatura')
+    else:
+        currentConditionResponse = json.loads(rTempLocation.text)
+        infoClima = {}
+        infoClima['temperature'] = str(currentConditionResponse[0]['Temperature']['Metric']['Value'])+' '+currentConditionResponse[0]['Temperature']['Metric']['Unit']
+        infoClima['climaTexto'] = currentConditionResponse[0]['WeatherText']
+        infoClima['nomeLocal'] = nomeLocal
 
-rTempLocation  = requests.get(geoConditionURL)
+        return infoClima
 
-if rTempLocation.status_code !=200:
-    print('Não foi possível conectar ao serviço de temperatura')
-else:
-    r3 = json.loads(rTempLocation.text)
-    temperature = str(r3[0]['Temperature']['Metric']['Value'])+' '+r3[0]['Temperature']['Metric']['Unit']
-    climaTexto = r3[0]['WeatherText']
-    print(temperature +' -> '+ climaTexto)
+## Inicio do Programa
+
+coordenadas = pegarCoordenadas()
+
+info = pegarCodigo(coordenadas['latitude'], coordenadas['longitude'])
+
+clima = pegarTempoAgora(info['codGeoPosition'], info['nomeLocal'])
+
+print(pt.pprint(clima))
