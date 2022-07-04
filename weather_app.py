42eed4999b117e27bf831754bd2269bc71ce9bea
Jean Alves Rocha <96813984+JeanAlvesR@users.noreply.github.com>
2022-07-04 10:46:15 -0300

    Utilizando dict multi-dimensionais para pegar pais, estado e cidade. O código foi pego também para ser utilizado na outra API

--- weather_app.py
+++ weather_app.py
@@ -10,16 +10,26 @@ if(r.status_code!=200):
     print('Não foi possível obter a localização.')
     exit()
 else:
-    #json.loads converte o r.text, que é uma str, para um dicionário: chave-valor.
+    ##json.loads converte o r.text, que é uma str, para um dicionário: chave-valor.
     localizacao = json.loads(r.text)
-    #print(pt.pprint(localizacao))
+    ##print(pt.pprint(localizacao))
     latitude = localizacao['geoplugin_latitude'] 
     longitude = localizacao['geoplugin_longitude']
-    print('Latitude:',latitude,'Logitude:',longitude) 
 
-rGeoPosition = requests.get('http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey='+accuweatherAPIKey+'&q='+latitude+'%2C%20'+longitude+'&language=pt-br')
+#Utilizando API para descobrir o código referente a latitude e longitude.
+localizationAPI = 'http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey='+accuweatherAPIKey+'&q='+latitude+'%2C%20'+longitude+'&language=pt-br'
+
+rGeoPosition = requests.get(localizationAPI)
 
 if(rGeoPosition.status_code!=200):
     print('Não foi possível obter a localização')
 else:
-    print(pt.pprint(rGeoPosition.text))
+    #só peguei o código e a cidade, estado e país
+    r2 = json.loads(rGeoPosition.text)
+    
+    nomeLocal = r2['LocalizedName'] + ', ' + r2['AdministrativeArea']['LocalizedName'] + '. ' + r2['Country']['LocalizedName'] +'.'
+
+    codGeoPositon = r2['Key']
+    print('Local:',nomeLocal)
+    print('Codigo:',codGeoPositon)
+
