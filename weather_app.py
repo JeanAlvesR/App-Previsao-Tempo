8fa0fc4fdd208e70b34ee1d81c6c876aca5f3857
Jean Alves Rocha <96813984+JeanAlvesR@users.noreply.github.com>
2022-07-04 10:18:33 -0300

    Utilizando API de localizacao com base na longitude e latitude

--- weather_app.py
+++ weather_app.py
@@ -1,10 +1,14 @@
 import requests
 import json
 import pprint as pt
+
+accuweatherAPIKey = 'JMXCPnfdkcPbmHyJ3EUyQ48dZe3bYZVm'
+
 r = requests.get('http://www.geoplugin.net/json.gp')
 
 if(r.status_code!=200):
     print('Não foi possível obter a localização.')
+    exit()
 else:
     #json.loads converte o r.text, que é uma str, para um dicionário: chave-valor.
     localizacao = json.loads(r.text)
@@ -12,3 +16,10 @@ else:
     latitude = localizacao['geoplugin_latitude'] 
     longitude = localizacao['geoplugin_longitude']
     print('Latitude:',latitude,'Logitude:',longitude) 
+
+rGeoPosition = requests.get('http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey='+accuweatherAPIKey+'&q='+latitude+'%2C%20'+longitude+'&language=pt-br')
+
+if(rGeoPosition.status_code!=200):
+    print('Não foi possível obter a localização')
+else:
+    print(pt.pprint(rGeoPosition.text))
